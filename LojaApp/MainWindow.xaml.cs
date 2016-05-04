441726b6fdcb4a5f56bfa34b5ecc83185363ad96
laviniameds <laviniameds@icloud.com>
2016-05-03 22:57:13 -0300

    first

---
+++ LojaApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LojaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CadFabricantesClick(object sender, RoutedEventArgs e)
+        {
+            WndCadFabricante w = new WndCadFabricante();
+            w.Show();
+        }
+
+        private void CadVeiculosClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SairClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsVeiculosClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LojaApp/WndCadFabricante.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LojaApp
+{
+    /// <summary>
+    /// Interaction logic for WndCadFabricante.xaml
+    /// </summary>
+    public partial class WndCadFabricante : Window
+    {
+        public WndCadFabricante()
+        {
+            InitializeComponent();
+        }
+
+        private LojaDataContext dc = new LojaDataContext();
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            var r = from f in dc.Fabricantes select new { f.Id, f.Descricao };
+            dataGrid.ItemsSource = null;
+            dataGrid.ItemsSource = r.ToList();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            Fabricante f = new Fabricante();
+            f.Id = int.Parse(txtId.Text);
+            f.Descricao = txtDes.Text;
+            dc.Fabricantes.InsertOnSubmit(f);
+            dc.SubmitChanges();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Fabricante r = (from f in dc.Fabricantes where f.Id == int.Parse(txtId.Text)select f).Single();
+            r.Descricao = txtDes.Text;
+            dc.SubmitChanges();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Fabricante r = (from f in dc.Fabricantes where f.Id == int.Parse(txtId.Text) select f).Single();
+            dc.Fabricantes.DeleteOnSubmit(r);
+            dc.SubmitChanges();
+        }
+    }
+}
